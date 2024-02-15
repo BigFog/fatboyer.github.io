fd2af2932bf56a5f339e1bb0db28dbb682cbeffa
mi <1042811368@qq.com>
2024-02-15 23:35:25 +0800

    Site updated: 2024-02-15 23:35:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2021/03/06/PWN学习—[XMAN] level2 (x32)/","2024/02/04/hello-world/"];function toRandomPost(){
+var posts=["2024/02/15/IOT/小白固件提取的踩坑之旅/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
