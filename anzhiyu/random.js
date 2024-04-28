b4816c03e429cec977880cac13beeeeca0ae9edc
mi <1042811368@qq.com>
2024-04-29 00:28:28 +0800

    Site updated: 2024-04-29 00:28:27

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/15/IOT/小白固件提取的踩坑之旅/","2024/03/02/WEB/小白学WEB安全01—信息收集/"];function toRandomPost(){
+var posts=["2024/04/28/WEB/小白学WEB安全01—信息收集/","2024/02/15/IOT/小白固件提取的踩坑之旅/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
